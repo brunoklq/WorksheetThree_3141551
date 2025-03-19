7c71ed32664b8f3a30a5a7e74b1300861ef0ef8a
Bruno Kennedy <bruno.klqueiroz@gmail.com>
2025-03-19 19:17:26 +0000

    added new attributes and methods for class Right and triangle,

--- src/griffith/RightAngledTriangle.java
+++ src/griffith/RightAngledTriangle.java
@@ -1,5 +1,52 @@
 package griffith;
 
-public class RightAngledTriangle {
+public class RightAngledTriangle extends shape {
+
+	private double height;
+	private double width;
+	
+	
+	public RightAngledTriangle(String name, double height, double width) {
+		super(name);
+		this.height = height;
+		this.width = width;
+		
+	}
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double perimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	public String toString() {
+		
+		return "Width: " + width + " height: " + height + "is: " +super.toString();
+	}
+	
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+	
 
 }
