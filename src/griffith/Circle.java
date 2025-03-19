4d7a750389e725f9f2ba410608d288aba41789b5
Bruno Kennedy <bruno.klqueiroz@gmail.com>
2025-03-19 19:04:30 +0000

    added new attributes for class extends Circle to shape

--- src/griffith/Circle.java
+++ src/griffith/Circle.java
@@ -1,5 +1,41 @@
 package griffith;
 
-public class Circle {
+public class Circle extends shape {
+	
+	
+	private double radius;
+
+	public Circle(String name, double radius) {
+		super(name);
+		this.radius = radius;
+	}
+	
+	
+	@Override
+	public double area() {
+	
+		return 0;
+	}
+
+	@Override
+	public double perimeter() {
+		
+		return 0;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	
+	public String toString() {
+		
+		return super.toString() + "The Radius: " + radius;
+	}
+	
 
 }
