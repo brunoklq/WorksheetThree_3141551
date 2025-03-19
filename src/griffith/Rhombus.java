2be74f609c5a82977410877158185382d18692b1
Bruno Kennedy <bruno.klqueiroz@gmail.com>
2025-03-19 19:11:50 +0000

    added new attributes and methods for extends class Rhombus

--- src/griffith/Rhombus.java
+++ src/griffith/Rhombus.java
@@ -1,5 +1,42 @@
 package griffith;
 
-public class Rhombus {
+public class Rhombus extends shape {
+	
+	
+	private double D_1;
+	private double D_2;
+	private double S;
+	
+	public Rhombus(String name, double D_1, double D_2, double S) {
+		super(name);
+		this.D_1 = D_1;
+		this.D_2 = D_2;
+		this.S = S;
+		
+	
+	}
+
+	@Override
+	public double area() {
+	
+		return 0;
+	}
+
+	@Override
+	public double perimeter() {
+	
+		return 0;
+	}
+
+	
+	
+	public String toString() {
+		
+		
+		return super.toString() + "Diagonal: " + D_1 + " and " + D_2 + "Side: " + S;
+	}
+	
+	
+	
 
 }
