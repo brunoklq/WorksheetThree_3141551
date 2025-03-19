fda2ea83c1d157eae542b9631197c2ba119d2cc2
Bruno Kennedy <bruno.klqueiroz@gmail.com>
2025-03-19 18:44:52 +0000

    added the elements inside of class as constructor methods and attributes, new features added

--- src/griffith/shape.java
+++ src/griffith/shape.java
@@ -1,5 +1,45 @@
 package griffith;
 
-public class shape {
+abstract class shape {
 
+	private String name;
+	
+
+	public shape(String name) {
+		
+		this.name = name;
+	}
+	
+	
+	public void setname(String name) {
+		
+		this.name = name;
+		
+	}
+	
+
+	
+	public abstract double area();
+	
+	
+	
+	public abstract double perimeter();
+	
+
+	
+	public String getName() {
+		
+		return name;
+	}
+	
+	
+	
+	public String toString() {
+		
+		
+		return "The Shape: " + name;
+	}
+	
+	
+	
 }
