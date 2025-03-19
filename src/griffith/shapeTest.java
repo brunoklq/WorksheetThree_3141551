adc5ffe4bc0ff7d3615276f74aa888cfce441538
Bruno Kennedy <bruno.klqueiroz@gmail.com>
2025-03-19 22:31:07 +0000

    implementation of testIntegration unit

--- src/griffith/shapeTest.java
+++ src/griffith/shapeTest.java
@@ -1,6 +1,9 @@
 package griffith;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
@@ -28,7 +31,7 @@ public class shapeTest {
 		assertEquals("The Shape: Rhombus Diagonal: 4.0 and 5.0 Side: 3.0 ", rhombus.toString());
 		
 		assertEquals((4*5)/2.0, rhombus.area(), epsilon);
-		assertEquals(4*3, rhombus.perimeter(), epsilon)
+		assertEquals(4*3, rhombus.perimeter(), epsilon);
 		
 		
 	}
@@ -43,6 +46,33 @@ public class shapeTest {
 		assertEquals((3*4) / 2.0, test.area(), epsilon);
 		assertEquals(3+4+5, test.perimeter(), epsilon);
 		
+		
+	}
+	
+	
+	@Test
+	public void testIntegration() {
+		
+		
+		ArrayList<shape> shapes = new ArrayList<>();
+		
+		shapes.add(new Circle ("Circle_1", 3.5));
+		shapes.add(new Circle("Circle_2", 2.0));
+		shapes.add(new Rhombus("Rombus_1", 4,5,3));
+		shapes.add(new Rhombus("Rhombus_2,",6,8,5));
+		shapes.add(new RightAngledTriangle("test_1", 3,4));
+		shapes.add(new RightAngledTriangle("test_2", 5,12));
+		
+		
+		for(shape shape: shapes) {
+			
+			double area = shape.area();
+			double perimeter = shape.perimeter();
+			
+			assertTrue(area >=0);
+			assertTrue(perimeter >=0);
+			
+		}
 		
 	}
 	
